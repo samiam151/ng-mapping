6e8b2a0a893d37941bda96e83435320d3c088f1c
Nicolas Sam <nsam@whereoware.com>
2017-03-17 17:28:33 -0400

    Continuing taxonomy...

--- node/tax.js
+++ node/tax.js
@@ -1,13 +1,11 @@
 const fs = require('fs')
 const reuters = require('thomson-reuters-trbc')
+const btr = require('./breakTaxonomyRoot')
 
 const dataUrl = "C:/Projects/ng-map/src/assets/data.json"
 
 fs.readFile(dataUrl, (err, data) => {
-    let businesses = JSON.parse(data.toString())
-    console.log(reuters.all())
-
-    // businesses.forEach(business => {
-
-    // })
+    let businesses = JSON.parse(data.toString()),
+        taxonomyRoot = reuters.all(),
+        businessTree = btr.makeTree(taxonomyRoot);
 })
